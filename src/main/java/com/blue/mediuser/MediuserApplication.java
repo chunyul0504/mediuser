8357fc1f784737748e2c283700ea1606f5a511d4
choi sujin <sjchoi@kanak.co.kr>
2022-06-21 16:19:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/blue/mediuser/MediuserApplication.java
@@ -0,0 +1,13 @@
+package com.blue.mediuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediuserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediuserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blue/mediuser/MediuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue.mediuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediuserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
